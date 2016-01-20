4ec0d4c9d5c2d157e238e318f91734b7a5a31707
nicothebrush <nicola.riolini@gmail.com>
2016-01-20 11:50:08 +0100

    Check this out!

--- csv_order_import_base/base.py
+++ csv_order_import_base/base.py
@@ -124,10 +124,12 @@ class SaleOrder(orm.Model):
         }
 
 class LogImportation(orm.Model):
+    ''' Add *many relation field
+    '''
     _inherit = 'log.importation'
     
     _columns = {
-        'order_ids': fields.one2many('sale.order', 'importation_id', 'Order'), 
+        #'order_ids': fields.one2many('sale.order', 'importation_id', 'Order'), 
         }
 
 class ResPartner(orm.Model):
