a08cd6a3b118e429a7236fecb1bf4ca31d90e355
nicothebrush <nicola.riolini@gmail.com>
2016-02-15 13:11:32 +0100

    translation

--- order_wizard_report/__openerp__.py
+++ order_wizard_report/__openerp__.py
@@ -18,7 +18,7 @@
 ###############################################################################
 
 {
-    'name': 'Procurement report wizard',
+    'name': 'Order report wizard',
     'version': '0.1',
     'category': '',
     'description': '''        
