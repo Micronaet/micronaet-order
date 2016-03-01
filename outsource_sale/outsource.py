a5ca7609f908b75d87235a1199606aabd9ad0cda
nicothebrush <nicola.riolini@gmail.com>
2016-03-01 15:41:14 +0100

    Start write 2 procedure for link orders pass file

--- outsource_sale/outsource.py
+++ outsource_sale/outsource.py
@@ -21,6 +21,8 @@ import os
 import sys
 import logging
 import openerp
+import pickle
+import tempfile
 import openerp.netsvc as netsvc
 import openerp.addons.decimal_precision as dp
 from openerp.osv import fields, osv, expression, orm
@@ -38,41 +40,140 @@ from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
 
 _logger = logging.getLogger(__name__)
 
+class ResCompany(orm.Model):
+    """ Model name: ResCompany
+    """    
+    _inherit = 'res.company'
+    
+    _columns = {
+        'outsourced_partner_id': fields.many2one(
+            'res.partner', 'Outsourced partner', 
+            help='Partner linked as company ref. (used in order header)', 
+            required=False),     
+        }    
+
 class SaleOrder(orm.Model):
     """ Model name: Sale order
-    """
-    
+    """    
     _inherit = 'sale.order'
 
-    # ----------------------------------------
-    # Function called from master DB (transit)
-    # ----------------------------------------
-    #def erpeek_stock_movement_inventory_data(self, cr, uid, product_ids, 
-    #        debug_f):
-    #    ''' Function called by Erpeek with dict passed by pickle file
-    #    '''
-    #    pickle_file = '/home/administrator/photo/dicts.pickle' # TODO 
-    #    debug_file = open(debug_f, 'a')    
-    #    
-    #    pickle_f = open(pickle_file, 'r')        
-    #    dicts = pickle.load(pickle_f)
-    #    pickle_f.close()
-    #    
-    #    res = self.stock_movement_inventory_data(cr, uid, 
-    #        product_ids, True, debug_file, dicts, context=None)#
+    # -------------------------------------------------------------------------
+    #                             REMOTE PROCEDURE: 
+    # -------------------------------------------------------------------------
+    def erpeek_stock_order_in(self, cr, uid, pickle_file):
+        ''' Read temp file and get data
+        '''
+        pickle_f = open(pickle_file, 'r')
+        order_dict = pickle.load(pickle_f)
+        pickle_f.close()
+        return order_dict
 
-    #    pickle_f = open(pickle_file, 'w')
-    #    pickle.dump(dicts, pickle_f)
-    #    pickle_f.close()
-    #    debug_file.close()
-    #    return res
-    
+    def create_order_outsource(self, cr, uid, pickle_file, context=None):
+        ''' XMLRPC procedure for import order in current company 
+            @return esit        
+        '''
+        sol_pool = self.pool.get('sale.order.line')
+        # Read dictionary passed:
+        order_dict = self.erpeek_stock_order_in(cr, uid, pickle_file)
+        
+        # ---------------------------------------------------------------------
+        #                              HEADER
+        # ---------------------------------------------------------------------
+        # Create order if not present:
+        name = order_dict['header'].name
+        order_ids = self.search(cr, uid, [], context=context)
+        if order_ids:
+            return _('Order jet present! Delete and reimport if not started!')
+        
+        partner_id = 0
+        data = self.onchange_partner_id(cr, uid, False, partner_id, 
+            context=context).get('value', {})        
+        data.update({
+            'partner_id': partner_id, 
+            'linked': True, # as outsource            
+            })
+        order_id = self.create(cr, uid, data, context=context)
+
+        # ---------------------------------------------------------------------
+        #                                LINE
+        # ---------------------------------------------------------------------
+        for line in order_dict['line']:
+            # TODO onchange event?
+            sol_pool.create(cr, uid, {
+                
+                }, context=context)
+
+        try:
+            os.remove(res)
+        except:
+            pass # do nothing
+
+        return False # no error    
+        
+    # -------------------------------------------------------------------------
+    #                             MASTER PROCEDURE: 
+    # -------------------------------------------------------------------------
+    def erpeek_stock_order_out(self, cr, uid, data):
+        ''' Function called by Erpeek with dict passed by pickle file
+        '''
+        pickle_f = tempfile.NamedTemporaryFile(delete=False)
+        pickle.dump(dicts, pickle_f)
+        res = f.name 
+        pickle_f.close()
+        return res
+
     def create_order_outsource(self, cr, uid, ids, context=None):
         ''' Create order in other company
         '''
+        assert len(ids) == 1, 'Call only for once'
+        
+        order_dict = {}
+        for order in self.browse(cr, uid, ids, context=context):
+            order_dict['header'] = {
+                'name': order.name,
+                'date_order': order.date_order,
+                'date_deadline': order.date_deadline,
+                'date_confirm': order.date_confirm,
+                'client_order_ref': order.client_order_ref,
+                'note': _(
+                    'Imported order\n Partner: %s [%s]\nDestination:\n' + \
+                    'Total: %s [Taxed: %s]\nCustomer ref.:'
+                    ) % (
+                        order.partner_id.name,
+                        order.partner_id.sql_customer_code,
+                        order.destination_partner_id.name \
+                            if order.destination_partner_id else '/', 
+                        order.amount_untaxed,
+                        order.amount_total,               
+                        order.client_order_ref,             
+                    )                
+                }
+            order_dict['line'] = []
+            for line in order.order_line:
+                if not line.outsource:
+                    continue
+                order_dict['line'].append({
+                    line.product_id.default_code,
+                    line.product_uom_qty,
+                    line.date_deadline,                    
+                    })    
+                    
+        # Write pickle file:        
+        pickle_file = self.erpeek_stock_order_out(
+            cr, uid, order_dict, context=context) 
+        
+        # Call XML RPC import procedure:
+        #esit = create_order_outsource(cr, uid, pickle_file)
+        # if esit # write outsource
         
         return True
         
+    _columns = {
+        'outsource': fields.boolean('Outsource order', 
+            help='This order will be outsourced'),
+        'linked': fields.boolean('Linked order', 
+            help='This order has an order in other company'),
+        }
     
 
 class SaleOrderLine(orm.Model):
