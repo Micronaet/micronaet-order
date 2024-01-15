a52fde21263a51419132193ee6645210eac85089
nicothebrush <nicola.riolini@gmail.com>
2024-01-15 11:01:40 +0100

    generic bugfix (fast message)

--- order_wizard_report/wizard/wizard_order_report.py
+++ order_wizard_report/wizard/wizard_order_report.py
@@ -32,31 +32,33 @@ from openerp import SUPERUSER_ID, api
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
 _logger = logging.getLogger(__name__)
 
+
 class SaleOrderGeneralReportWizard(orm.TransientModel):
-    ''' Procurements depend on sale
-    '''    
+    """ Procurements depend on sale
+    """
     _name = 'sale.order.general.report.wizard'
     _description = 'Sale general wizard'
-    
+
     # --------------
     # Button events:
     # --------------
     def print_report(self, cr, uid, ids, context=None):
-        ''' Redirect to report passing parameters
-        ''' 
+        """ Redirect to report passing parameters
+        """
         wiz_proxy = self.browse(cr, uid, ids)[0]
-            
+
         datas = {}
-        datas['wizard'] = True # started from wizard
-        
+
+        datas['wizard'] = True  # started from wizard
+
         datas['discount_limit'] = ''
         datas['order_status'] = ''
         if wiz_proxy.report_type == 'deadlined':
@@ -66,7 +68,7 @@ class SaleOrderGeneralReportWizard(orm.TransientModel):
         elif wiz_proxy.report_type == 'discount':
             report_name = 'mx_order_discount_line_report'
 
-            # Translate discount as number            
+            # Translate discount as number
             discount_limit = wiz_proxy.discount_limit or False
             if discount_limit:
                 res = self.pool.get(
@@ -75,19 +77,20 @@ class SaleOrderGeneralReportWizard(orm.TransientModel):
                 datas['discount_limit'] = res.get('discount', 0)
             else:
                 datas['discount_limit'] = False
-            
-        else: # 'line'    
+
+        else:  # 'line'
             report_name = 'mx_order_list_line_report'
 
+        datas['statistic_category'] = wiz_proxy.statistic_category_id or False
         datas['partner_id'] = wiz_proxy.partner_id.id or False
         datas['fiscal_position'] = wiz_proxy.fiscal_position
         datas['from_date'] = wiz_proxy.from_date or False
         datas['to_date'] = wiz_proxy.to_date or False
         datas['from_deadline'] = wiz_proxy.from_deadline or False
         datas['to_deadline'] = wiz_proxy.to_deadline or False
-        #datas['only_remain'] = wiz_proxy.only_remain
+        # datas['only_remain'] = wiz_proxy.only_remain
         datas['data_type'] = wiz_proxy.data_type
-        datas['report_type'] = wiz_proxy.report_type        
+        datas['report_type'] = wiz_proxy.report_type
         datas['data_sort'] = wiz_proxy.data_sort
         datas['order_status'] = wiz_proxy.order_status
 
@@ -108,18 +111,21 @@ class SaleOrderGeneralReportWizard(orm.TransientModel):
             ('line', 'Order line deadline'),
             ('discount', 'Discount check'),
             ('extract', 'Extract order'),
-            #('grouped', 'Order grouped by frame'),
+            # ('grouped', 'Order grouped by frame'),
             ], 'Report type', required=True),
+        'statistic_category_id': fields.many2one(
+            'statistic.category', 'Categoria statistica'),
         'partner_id': fields.many2one('res.partner', 'Partner'),
-        'only_remain':fields.boolean('Only remain', 
+        'only_remain': fields.boolean(
+            'Only remain',
             help='Show only element to produce'),
-        
+
         'from_date': fields.date('From', help='Date >='),
         'to_date': fields.date('To', help='Date <'),
         'from_deadline': fields.date('From deadline', help='Date deadline >='),
         'to_deadline': fields.date('To deadline', help='Date deadline <'),
 
-        'discount_limit': fields.char('Discount', size=64), 
+        'discount_limit': fields.char('Discount', size=64),
         'order_status': fields.selection([
             ('all', 'All order'),
             ('open', 'Open order'),
@@ -138,15 +144,14 @@ class SaleOrderGeneralReportWizard(orm.TransientModel):
             ('s', 'S(suspended)'),
             ], 'Data type', required=True),
         }
-        
+
     _defaults = {
         'fiscal_position': lambda *x: 'all',
         'report_type': lambda *x: 'deadlined',
         'only_remain': lambda *x: True,
-        #'to_date': datetime.now().strftime(DEFAULT_SERVER_DATE_FORMAT),
+        # 'to_date': datetime.now().strftime(DEFAULT_SERVER_DATE_FORMAT),
         'data_sort': lambda *x: 'number',
         'data_type': lambda *x: 'oc',
         'discount_limit': lambda *x: '50 + 20',
         'order_status': lambda *x: 'open',
         }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
