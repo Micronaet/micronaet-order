081761608ac2ad6e1807a7cf951f3898cde6a84a
nicothebrush <nicola.riolini@gmail.com>
2024-09-30 10:45:59 +0200

    generic bugfix (fast message)

--- script/cron_sale_order_line_store/mode_line.py
+++ script/cron_sale_order_line_store/mode_line.py
@@ -28,7 +28,7 @@ import ConfigParser
 # Read configuration parameter:
 # -----------------------------------------------------------------------------
 # From config file:
-cfg_file = os.path.expanduser('../openerp.cfg')
+cfg_file = os.path.expanduser('../openerp.fia')
 
 config = ConfigParser.ConfigParser()
 config.read([cfg_file])
